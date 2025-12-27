d4c55caed53455ecc0682d368852eb2338865b99
Yash Lodhi <yashlodhi2006@gmail.com>
2025-12-27 23:08:42 +0530

    feat: add payment gateway (payu)

---
+++ campus-delivery/app/api/payment-gateway/initiate/route.js
@@ -0,0 +1,43 @@
+import { NextResponse } from "next/server";
+import { payuClient } from "../../../../lib/paymentGateway";
+
+export async function POST(req) {
+  try {
+    const {
+      amount,
+      firstName,
+      email,
+      phone,
+      orderId,
+      address,
+      pg,
+      productInfo = "payu",
+      city = "Delhi",
+      state = "Delhi",
+      country = "India",
+      surl = "http://localhost:3000/receipt?orderId=" + orderId,
+      furl = "http://localhost:3000/payment-failed?orderId=" + orderId,
+    } = await req.json();
+
+    const form = payuClient.paymentInitiate({
+      txnid: orderId,
+      amount: `${amount.toString()}.00`,
+      firstname: firstName,
+      email,
+      phone,
+      surl,
+      furl,
+      city,
+      state,
+      address,
+      productinfo: productInfo,
+      country,
+      enforce_paymethod: pg,
+    });
+
+    return NextResponse.json({ success: true, form });
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json({ success: false }, { status: 500 });
+  }
+}
--- campus-delivery/lib/db.js
+++ campus-delivery/lib/db.js
@@ -1,19 +1,19 @@
-// import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-// const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI;
 
-// if (!MONGODB_URI) throw new Error("Missing Mongo URI")
+if (!MONGODB_URI) throw new Error("Missing Mongo URI");
 
-// let cached = global.mongoose
-// if (!cached) cached = global.mongoose = { conn: null, promise: null }
+let cached = global.mongoose;
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
 
-// export async function connectDB() {
-//   if (cached.conn) return cached.conn
+export async function connectDB() {
+  if (cached.conn) return cached.conn;
 
-//   if (!cached.promise) {
-//     cached.promise = mongoose.connect(MONGODB_URI)
-//   }
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI);
+  }
 
-//   cached.conn = await cached.promise
-//   return cached.conn
-// }
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
